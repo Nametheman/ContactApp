c1b3bcf78d201d5ce95ef0f81eadd7a552e26a75
nametheman <owoemma59@gmail.com>
2022-08-26 10:34:56 +0100

    commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import Contacts from "./ContactList/Contacts";
+import ContactEntry from "./NewContact/ContactEntry";
+import "./Body.css";
 
 function App() {
+  const items = JSON.parse(localStorage.getItem("contacts"));
+  let newItems = [];
+  if (items && items.length !== 0) {
+    newItems = items.map((item) => {
+      return [
+        {
+          ...item,
+          id: Math.random().toString(),
+        },
+      ];
+    });
+  }
+  newItems = [];
+  const [contacts, SetContacts] = useState(newItems);
+  const addContactHandler = (contact) => {
+    SetContacts((prevContact) => {
+      return [contact, ...prevContact];
+    });
+  };
+  localStorage.setItem("contacts", JSON.stringify(contacts));
+
+  const deleteContactHandler = (contact_id) => {
+    SetContacts((prevContact) => {
+      const contactAfterDelete = prevContact.filter(
+        (contact) => contact.id !== contact_id
+      );
+      return contactAfterDelete;
+    });
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <section className="container">
+        <ContactEntry onContactEntry={addContactHandler} />
+        <Contacts items={contacts} onDelete={deleteContactHandler} />
+      </section>
     </div>
   );
 }
